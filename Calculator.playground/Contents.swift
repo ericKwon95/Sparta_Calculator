c3e392c06b0f487f89387112be880f381b9eea6a
ericKwon95 <ericyong95@gmail.com>
2024-10-30 09:11:29 +0900

    feat: Calculator 기능 구현
    - 덧셈 / 뺄셈 / 곱셈 / 나눗셈 연산 구현

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,3 +1,25 @@
 import Foundation
 
+class Calculator {
+    
+    func calculate(operator op: String, firstNumber: Double, secondNumber: Double) -> Double {
+        switch op {
+        case "+":
+            return firstNumber + secondNumber
+        case "-":
+            return firstNumber - secondNumber
+        case "*":
+            return firstNumber * secondNumber
+        case "/":
+            return firstNumber / secondNumber
+        default:
+            return 0
+        }
+    }
+}
 
+let calculator = Calculator()
+let addResult = calculator.calculate(operator: "+", firstNumber: 10, secondNumber: 20)
+let subResult = calculator.calculate(operator: "-", firstNumber: 10, secondNumber: 20)
+let mulResult = calculator.calculate(operator: "*", firstNumber: 10, secondNumber: 20)
+let divResult = calculator.calculate(operator: "/", firstNumber: 10, secondNumber: 20)
