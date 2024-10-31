402416269d9df3fb6171457c8bca976f2992d03d
ericKwon95 <ericyong95@gmail.com>
2024-10-31 13:05:11 +0900

    comment: 주석 작성

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,33 +1,39 @@
 import Foundation
 
+/// calculate 함수의 추상화를 담당하는 프로토콜
 protocol AbstractOperation {
     func calculate(_ a: Double, _ b: Double) -> Double
 }
 
+/// 덧셈 연산을 담당하는 클래스
 class AddOperation: AbstractOperation {
     func calculate(_ a: Double, _ b: Double) -> Double {
         return a + b
     }
 }
 
+/// 뺄셈 연산을 담당하는 클래스
 class SubstractOperation: AbstractOperation {
     func calculate(_ a: Double, _ b: Double) -> Double {
         return a - b
     }
 }
 
+/// 곱셈 연산을 담당하는 클래스
 class MultiplyOperation: AbstractOperation {
     func calculate(_ a: Double, _ b: Double) -> Double {
         return a * b
     }
 }
 
+/// 나눗셈 연산을 담당하는 클래스
 class DivideOperation: AbstractOperation {
     func calculate(_ a: Double, _ b: Double) -> Double {
         return a / b
     }
 }
 
+/// 나머지 연산을 담당하는 클래스
 class RemainderOperation: AbstractOperation {
     func calculate(_ a: Double, _ b: Double) -> Double {
         guard b != 0 else {
@@ -37,8 +43,15 @@ class RemainderOperation: AbstractOperation {
     }
 }
 
+/// 계산기 클래스
 class Calculator {
     
+    /// 주입받은 AbstractOperation 타입을 준수해는 객체의 calculate 함수를 이용해 연산을 수행하는 함수
+    /// - Parameters:
+    ///   - op: 어떤 연산을 수행할 것인지 나타내는 AbstractOperation 타입의 인자
+    ///   - firstNumber: 연산을 진행할 첫 번째 숫자
+    ///   - secondNumber: 연산을 진행할 두 번째 숫자
+    /// - Returns: 연산 결과
     func calculate(operation op: AbstractOperation, firstNumber: Double, secondNumber: Double) -> Double {
         op.calculate(firstNumber, secondNumber)
     }
